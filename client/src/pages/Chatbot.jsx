9f36a1ec8ed7526878fdbd1f9ef01ea8fd10b447
niazbuoy08 <rahmanniaz29@gmail.com>
2025-01-04 04:14:42 +0600

    FrontendUpdate

--- client/src/pages/Chatbot.jsx
+++ client/src/pages/Chatbot.jsx
@@ -71,7 +71,7 @@ const Chatbot = () => {
         {/* Title and Subtext Section */}
         <div className="text-center px-6 py-8">
           <h2 className="text-3xl font-bold text-gray-800 mb-6">
-            Welcome to BanglishBaba!
+            Welcome to Blingo!
           </h2>
           <p className="text-gray-600 italic">
             Use our AI-powered chatbot to transform Banglish into fluent Bangla.
